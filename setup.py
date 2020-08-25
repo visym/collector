4e8ab1b5e4c458d80d9f4374f0612d8bda324f94
Byrne <jeff@visym.com>
2020-08-25 14:03:02 -0400

    readme

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name='pycollector',
+    author='Visym Labs',
+    author_email='info@visym.com',
+    version=version,
+    packages=find_packages(),
+    description='Visym Collector',
+    long_description="Visym Collector",
+    long_description_content_type="text/markdown",
+    url='https://github.com/visym/collector',
+    download_url='https://github.com/visym/collector/archive/%s.tar.gz' % version,
+    install_requires=[
+        "vipy",
+    ],
+    keywords=['vision', 'learning', 'ML', 'CV'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
