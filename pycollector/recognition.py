fbcdeef8e0344ea7bb8e0063fab968507cc65abc
Jeffrey Byrne <jeff@visym.com>
2020-09-09 19:50:04 -0400

    resnet-50 3d

--- pycollector/recognition.py
+++ pycollector/recognition.py
@@ -14,6 +14,7 @@ from pycollector.model.pyvideoresearch.bases.resnet50_3d import ResNet503D, ResN
 class ActivityRecognition(object):
     def __init__(self):
         #weights = '/proj/diva3/visym/models/train_resnet50_3d_pretrain_pip50k_finetune_betterbox_meva/model_best.pth.tar' 
-        weightts = None
+        weights = 'resnet50_3d_pretrain_pip50k_finetune_betterbox_meva.pth.tar'
+        os.system('wget -c https://dl.dropboxusercontent.com/s/a0ouihgjxiwn2k8/resnet50_3d_pretrain_pip50k_finetune_betterbox_meva.pth.tar -O %s' % weights) 
         d = {k.replace('module.basenet.0.',''):v for (k,v) in torch.load(weights)['state_dict'].items()}
         self.net =  ResNet3D(Bottleneck3D, [3, 4, 6, 3], num_classes=53).load_state_dict(d)
