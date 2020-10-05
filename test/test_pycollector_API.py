3805e8d267e3e9f7154b6a612ac4f605d73c9237
Li <zla0368@str-mac-0904.str.corp>
2020-10-05 15:48:08 -0400

    Updated to add create collectioin

--- test/test_pycollector_API.py
+++ test/test_pycollector_API.py
@@ -12,35 +12,35 @@ _PASSWORD = os.environ['VISYM_COLLECTOR_PYTEST_PASSWORD']
 _PROGRAM_ID = 'MEVA'
 
 
-# @pytest.mark.parametrize("username, password", [(_USER_NAME, _PASSWORD)])
-# def test_get_user_video(username,password):
-#     """test get user video
-#     """
-#     # testing objects and functions 
-#     api = API(username=username, password=password)
-#     assert api.username == username
-#     assert api.is_token_expired() == False
+@pytest.mark.parametrize("username, password", [(_USER_NAME, _PASSWORD)])
+def test_get_user_video(username,password):
+    """test get user video
+    """
+    # testing objects and functions 
+    api = API(username=username, password=password)
+    assert api.username == username
+    assert api.is_token_expired() == False
 
 
-# @pytest.mark.parametrize("username, password, program_id", [(_USER_NAME, _PASSWORD, _PROGRAM_ID)])
-# def test_get_user_videos(username,password, program_id):
-#     """test get user video
-#     """
+@pytest.mark.parametrize("username, password, program_id", [(_USER_NAME, _PASSWORD, _PROGRAM_ID)])
+def test_get_user_videos(username,password, program_id):
+    """test get user video
+    """
     
-#     # testing objects and functions 
-#     api = API(username=username, password=password)
-#     project =  api.get_project(program_id=program_id)
-#     assert len(project) > 0
+    # testing objects and functions 
+    api = API(username=username, password=password)
+    project =  api.get_project(program_id=program_id)
+    assert len(project) > 0
     
-#     # show the df of project
-#     print(project.df)
+    # show the df of project
+    print(project.df)
 
-#     # fetch video
-#     videos = project.videos()
-#     assert len(videos) > 0
+    # fetch video
+    videos = project.videos()
+    assert len(videos) > 0
         
-#     # Download video 
-#     videos[0].download() 
+    # Download video 
+    videos[0].download() 
 
 
 
@@ -62,7 +62,6 @@ def test_new_collections(username,password, program_id):
     activities_shortnames = 'Thank you'
     objects = 'Person'
     cognito_username: 'test_cognito_username'
-
 
     api.new_collection(name, organization_name, program_name, project_name, description, activities, activity_short_names, objects, cognito_username)
 
