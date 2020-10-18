cf2007a37b592e8a96d7e079a8548999c053c6e2
zla0368 <zhongheng.li@stresearch.com>
2020-10-18 09:33:13 -0400

    Added test_delete_collections() to test_pycollector_API.py

--- pycollector/backend.py
+++ pycollector/backend.py
@@ -44,7 +44,7 @@ class API(User):
         return Project(program_id=program_id, last=n).last(n)
     
 
-    def new_collection(self, name, program_name, project_name, collection_description, activities, activity_short_names, objects, actor, creator_cognito_username, consent_overlay_text='Please select the record button, say "I consent to this video collection”'):
+    def new_collection(self, name, program_name, project_name, collection_description, activities, activity_short_names, objects, actor, consent_overlay_text='Please select the record button, say "I consent to this video collection”'):
 
         # Invoke Lambda function
         request = {'collection_name':name,
@@ -55,7 +55,7 @@ class API(User):
                     'actor' : actor,
                     'collection_description': collection_description,
                     'activity_short_names': activity_short_names, # comma separated string, no spaces
-                    'creator_cognito_username' : creator_cognito_username, 
+                    'creator_cognito_username' : self.username,
                     'consent_overlay_text': consent_overlay_text,
                     }
 
@@ -71,3 +71,27 @@ class API(User):
             custom_error = '\nException : failed to invoke jobs.\n'
             custom_error += 'Error : ' + str(e) + '\n'
             raise Exception(custom_error)
+
+    def delete_collection(self, collectionid):
+
+        # Invoke Lambda function
+        request = {'collectorid': self.username,
+                    'collectionid': collectionid
+                    }
+
+        # Invoke Lambda function
+        try:
+            response = self._lambda_client.invoke(
+            FunctionName='arn:aws:lambda:us-east-1:806596299222:function:pycollector_delete_collection',
+            InvocationType= 'RequestResponse',
+            LogType='Tail',
+            Payload=json.dumps(request),
+        )
+        except Exception as e:
+            custom_error = '\nException : failed to invoke jobs.\n'
+            custom_error += 'Error : ' + str(e) + '\n'
+            raise Exception(custom_error)
+
+
+
+
