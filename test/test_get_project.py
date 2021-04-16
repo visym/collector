a1b09c09ee3876d64741d7ed59005295ec7aa77f
zli <zli.ctr@kr.af.mil>
2021-04-16 18:28:04 -0400

    fix(python_api): update unit test

--- test/test_get_project.py
+++ test/test_get_project.py
@@ -6,29 +6,56 @@ from pytz import timezone
 import json
 
 # internal testing modules
-from pycollector.user import User
+# from pycollector.user import User
 from pycollector.project import Project
-import pycollector.globals
+
+import pycollector
+from pycollector.admin.globals import backend, org
 
 
-def test_get_project():
-    """ test get project by user
-    """
+# def test_get_project_str():
+#     """test get project by user"""
+
+#     # Set to target ENV
+#     # pycollector.globals.backend(env='test', org='str')
+
+#     # Set to target ENV
+#     pycollector.admin.globals.backend(env="test", org="str")
+
+#     # testing variiables
+#     username = os.environ["VISYM_COLLECTOR_PYTEST_EMAIL"]  # github secrets
+#     password = os.environ["VISYM_COLLECTOR_PYTEST_PASSWORD"]  # github secrets
+
+#     # testing objects and functions
+#     new_project_client = Project(program_id="MEVA", alltime=True, username=username, password=password)
+
+#     # Fetching Videos
+#     videos = new_project_client.videos()
+#     assert len(videos) >= 1
+
+#     # # quickshow video
+#     videos[0].quickshow()
+
+
+def test_get_project_visym():
+    """test get project by user"""
 
     # Set to target ENV
-    pycollector.globals.backend(env='test', org='str')
+    # pycollector.globals.backend(env='test', org='str')
+
+    # Set to target ENV
+    pycollector.admin.globals.backend(env="prod", org="visym")
 
     # testing variiables
-    username = os.environ['VISYM_COLLECTOR_PYTEST_EMAIL']  # github secrets
-    password = os.environ['VISYM_COLLECTOR_PYTEST_PASSWORD']  # github secrets
+    username = os.environ["VISYM_COLLECTOR_PYTEST_EMAIL"]  # github secrets
+    password = os.environ["VISYM_COLLECTOR_PYTEST_PASSWORD"]  # github secrets
 
-    # testing objects and functions 
-    new_project_client = Project(program_id="MEVA", alltime=True, username=username, password=password)
+    # testing objects and functions
+    new_project_client = Project(program_id="Practice", alltime=True, username=username, password=password)
 
     # Fetching Videos
     videos = new_project_client.videos()
     assert len(videos) >= 1
 
     # # quickshow video
-    # videos[0].quickshow()
-
+    videos[0].quickshow()
--- test/test_pycollector_API.py
+++ test/test_pycollector_API.py
@@ -4,50 +4,54 @@ from datetime import datetime, timedelta
 import pandas as pd
 from pytz import timezone
 import json
-from pycollector.backend import API 
+from pycollector.backend import API
 
 # testing variables
-_USER_NAME = os.environ['VISYM_COLLECTOR_PYTEST_EMAIL']
-_PASSWORD = os.environ['VISYM_COLLECTOR_PYTEST_PASSWORD']
-_PROGRAM_ID = 'MEVA'
-
+_USER_NAME = os.environ["VISYM_COLLECTOR_PYTEST_EMAIL"]
+_PASSWORD = os.environ["VISYM_COLLECTOR_PYTEST_PASSWORD"]
+_PROGRAM_ID = "MEVA"
 
 
 @pytest.mark.skip(reason="Skip testing for now")
 @pytest.mark.parametrize("username, password, program_id", [(_USER_NAME, _PASSWORD, _PROGRAM_ID)])
-def test_new_collections(username,password, program_id):
-    """test add  new collection
-    """
-    
-    # testing objects and functions 
+def test_new_collections(username, password, program_id):
+    """test add  new collection"""
+
+    # testing objects and functions
     api = API(username=username, password=password)
 
     # test to create new collection
-    name = 'Thank you'
-    program_name = 'ARSL'
-    project_name = 'ARSL_American Sign Language - Purchasing - 20201015'
-    actor = 'Person',
-    collection_description = 'ASL for asking some if the person needs help. \n\n The sign for \"help\" is made by forming a loose-thumb-A hand (or even an \"S\" hand) and lifting it with your other hand. Some people will tell you the \"A\" hand should be your right hand. Others will tell you it should be your left hand. The reality of the matter is if you look this sign up in a half-dozen different sources you are going to see it done several different ways. \n\n  HELP: You / Need Help / '
-    activities = 'thank you,very much'
-    activity_short_names = 'Thank you,Very much'
-    objects = 'Hands'
+    name = "Thank you"
+    program_name = "ARSL"
+    project_name = "ARSL_American Sign Language - Purchasing - 20201015"
+    actor = ("Person",)
+    collection_description = 'ASL for asking some if the person needs help. \n\n The sign for "help" is made by forming a loose-thumb-A hand (or even an "S" hand) and lifting it with your other hand. Some people will tell you the "A" hand should be your right hand. Others will tell you it should be your left hand. The reality of the matter is if you look this sign up in a half-dozen different sources you are going to see it done several different ways. \n\n  HELP: You / Need Help / '
+    activities = "thank you,very much"
+    activity_short_names = "Thank you,Very much"
+    objects = "Hands"
 
     # TODO return and response collection id
-    api.new_collection(name=name, program_name=program_name, project_name=project_name, collection_description=collection_description, activities=activities, activity_short_names=activity_short_names, objects=objects, actor=actor)
-
+    api.new_collection(
+        name=name,
+        program_name=program_name,
+        project_name=project_name,
+        collection_description=collection_description,
+        activities=activities,
+        activity_short_names=activity_short_names,
+        objects=objects,
+        actor=actor,
+    )
 
 
 @pytest.mark.skip(reason="Skip testing for now")
 @pytest.mark.parametrize("username, password, program_id", [(_USER_NAME, _PASSWORD, _PROGRAM_ID)])
-def test_delete_collections(username,password, program_id):
-    """test add  new collection
-    """
-    
-    # testing objects and functions 
+def test_delete_collections(username, password, program_id):
+    """test add  new collection"""
+
+    # testing objects and functions
     api = API(username=username, password=password)
-    collectionid = '17d47360-35dd-4dd2-9600-8ce196f1e299'
+    collectionid = "17d47360-35dd-4dd2-9600-8ce196f1e299"
 
     # test to delete new collectio
     # TODO return and response collection id
     api.delete_collection(collectionid=collectionid)
-
